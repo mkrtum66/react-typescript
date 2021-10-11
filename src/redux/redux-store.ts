6f2701e378b71a4871aafee646ee4e47e821ddef
mkrtum <mkrtum.abrahamyan66@gmail.com>
2021-10-11 14:41:25 +0400

    09 - React + TypeScript / Actions Creators, Generic, infer/ React JS

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -21,6 +21,10 @@ let rootReducer = combineReducers({
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
 
+type PropertyTypes<T> = T extends {[key: string]:  infer U} ? U : never
+
+export type InferActionsTypes<T extends {[key: string]:  (...args: any[]) => any} > = ReturnType<PropertyTypes<T>>
+
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunkMiddleware)));
